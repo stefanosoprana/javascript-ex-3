6fb49b3f49c6d09bfb4d457eb98af6b2f4ae924e
stefanosoprana <stefano.soprana@icloud.com>
2018-11-15 16:33:43 +0100

    alphabetic order

---
+++ main.js
@@ -0,0 +1,24 @@
+// Chiedi all’utente il cognome, inseriscilo in un
+// array con altri cognomi e stampa la lista
+// ordinata alfabeticamente. Scrivi a che
+// posizione della lista il nuovo utente si trova
+
+// chiedi all'utente il cognome
+
+var cognome = 'conti'
+
+// crea una lista di cognomi
+
+var lista_cognomi = ['verdi', 'garibaldi', 'mazzini'];
+
+console.log(lista_cognomi);
+
+// inserisci il cognome nella lista
+
+lista_cognomi.push(cognome);
+
+// mettere la lista in ordine alfabetico
+
+lista_cognomi.sort()
+
+// scrivere a che posizione della lista si trova il cognome
